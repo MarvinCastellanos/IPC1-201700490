601ac928d12fbcef74c2f9ed6551dfdb833826e9
Marvin Castellanos <madacast2@gmail.com>
2020-06-25 21:13:31 -0600

    Hoja de trabajo 3

---
+++ HT/HT3/CODIGO/HT3/src/ht3/HT3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ht3;
+
+/**
+ *
+ * @author Marvin
+ */
+public class HT3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HTres mostrar = new HTres();
+        mostrar.setVisible(true);
+        
+    }
+    
+}
---
+++ HT/HT3/README.md
@@ -0,0 +1,2 @@
+# Hoja de trabajo 3
+![alt text](https://github.com/MarvinCastellanos/IPC1-201700490/tree/master/HT/HT3/captura.PNG)
